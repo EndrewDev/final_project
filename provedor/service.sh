d3a16b5f4a0b0ecd3b28637b5998c79934268038
Endrew Sousa <endrew.developer@gmail.com>
2025-02-05 12:15:33 -0300

    melhorado sshell script

--- provedor/service.sh
+++ provedor/service.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-start_containers() {
-  echo "Iniciando containers..."
-  docker-compose up --build
-}
+echo "Verificando se os containers estão rodando..."
+docker ps -q --filter "name=proxy_container" --filter "name=dns_container" --filter "name=email" --filter "name=roundcube"
 
-stop_containers() {
-  echo "Parando e removendo containers e imagens..."
-  docker-compose down --volumes --rmi all
-}
-
-if [ "$1" == "start" ]; then
-  start_containers
-elif [ "$1" == "stop" ]; then
-  stop_containers
+if [ $? -eq 0 ]; then
+    echo "Containers estão em execução. Parando e removendo containers e imagens..."
+    docker-compose down -v --rmi all
+    docker system prune -af
 else
-  echo "Uso correto: $0 {start|stop}"
-  exit 1
+    echo "Nenhum container em execução."
 fi
+
+echo "Construindo e subindo os containers..."
+docker-compose up --build
+
+echo "Containers e serviços estão rodando!"
