9874cefaf8b2c5739bc813902a75432a69ef2f74
Endrew Sousa <endrew.developer@gmail.com>
2025-02-05 11:07:06 -0300

    consegui entra localhost do web, mas, o problema é acessa o usuário

--- provedor/webemail/config/config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config['db_dsnw'] = 'sqlite:////var/www/html/db/sqlite.db';
-$config['default_host'] = 'ssl://mail.exemplo.com';
-$config['smtp_server'] = 'tls://mail.exemplo.com';
-$config['smtp_user'] = '%u';
-$config['smtp_pass'] = '%p';
-$config['imap_auth_type'] = null;
-$config['smtp_auth_type'] = null;
-$config['enable_installer'] = false;
