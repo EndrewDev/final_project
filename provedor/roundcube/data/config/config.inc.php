8addfc7453b384b3becb2440530382742300605f
Endrew Sousa <endrew.developer@gmail.com>
2025-02-06 11:42:22 -0300

    trabalho final

--- provedor/roundcube/data/config/config.inc.php
+++ provedor/roundcube/data/config/config.inc.php
@@ -5,10 +5,5 @@
     $config['des_key'] = 'Wd6zn6c9A+DVnvks6GHxl2d3';
     $config['enable_spellcheck'] = true;
     $config['spellcheck_engine'] = 'pspell';
-
-    // Configuração do servidor IMAP
-    $config['default_host'] = 'imap_server';  // Substitua 'imap_server' pelo nome do contêiner ou IP
-    $config['default_port'] = 143;  // Se for IMAPS, use 993
-    
     include(__DIR__ . '/config.docker.inc.php');
     
